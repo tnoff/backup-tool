1f6a53398f4fd01dd72ef76d9ad4583b126e2c2e
tnoff <1326564+tnoff@users.noreply.github.com>
2019-11-24 04:33:53 +0000

    Combine common test functions into utils

--- tests/test_client.py
+++ tests/test_client.py
@@ -6,64 +6,9 @@ from backup_tool import utils
 from backup_tool.client import BackupClient
 from backup_tool.oci_client import ObjectStorageClient
 
-def from_file_config_mock(config_file, config_section):
-    return 'mock config'
-
-def to_dict_mock(object_data):
-    return vars(object_data)
-
-
-class MockObjectOne():
-    def __init__(self):
-        self.name = 'foo.csv'
-        self.size = 1234
-        self.md5 = "9i9RHkgTENq8BSSGzKmqxg=="
-        self.timeCreated = "2019-07-21T23:22:54.663000+00:00"
-
-
-class MockObjectTwo():
-    def __init__(self):
-        self.name = 'bar.csv'
-        self.size = 4561
-        self.md5 = "fnqonfqowenfAoGzKmqxg=="
-        self.timeCreated = "2019-07-24T23:23:54.663000+00:00"
-
-
-class MockObjectData():
-    def __init__(self, objects, start):
-        self.objects = objects
-        self.next_start_with = start
-
-
-class MockObjectResponse():
-    def __init__(self, status_code, data=None, objects=None, start=None):
-        self.status = status_code
-        # For list objects
-        if data:
-            self.data = data
-        else:
-            self.data = MockObjectData(objects, start)
-
-
-class MockOS():
-    def __init__(self, _config):
-        pass
-
-    def list_objects(self, namespace_name, bucket_name, **kwargs):
-        if kwargs.get('start') is None:
-            return MockObjectResponse(200, objects=[MockObjectOne()], start="bar.ods")
-        else:
-            return MockObjectResponse(200, objects=[MockObjectTwo()], start=None)
-
-    def put_object(self, namespace_name, bucket_name, object_name, put_object_body, **kwargs):
-        return MockObjectResponse(200)
-
-    def delete_object(self, namespace_name, bucket_name, object_name, **kwargs):
-        return MockObjectResponse(200)
-
+from tests.utils import MockOS, MockOS404, to_dict_mock
 
 class TestClient(unittest.TestCase):
-
     def test_client_basic_upload(self):
         with mock.patch("oci.config.from_file") as mock_config:
             mock_config.return_value = "mock config"
